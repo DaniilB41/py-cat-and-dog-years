d268489c80eab4765312c6600fc49cd97b6b4583
Daniil Baianov <bayanovdanil666@gmail>
2026-04-24 17:52:02 +0200

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,24 @@
+import pytest
+
+
 from app.main import get_human_age
 
 
-def test_should_return_converted_years() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(27, 27) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ]
+)
+def test_should_return_converted_years(
+        cat_age: int,
+        dog_age: int,
+        result: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result
